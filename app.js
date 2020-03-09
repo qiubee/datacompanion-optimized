b11bf0074b4522153d43336a73a450dcd2de5dba
qiubee <qiubees@hotmail.com>
2020-03-09 14:09:29 +0100

    add handlebars as templating engine

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
 app.get('/', function (req, res) { 
     res.send('Hello World!');
 });
