419b2ec86e582c9b370b1225098f506690f3c71e
qiubee <qiubees@hotmail.com>
2020-03-24 14:36:13 +0100

    setup service worker (cache & update assets)

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,38 @@
+// from example: https://serviceworke.rs/strategy-cache-and-update_service-worker_doc.html
+const CACHE = "datacompanion-v1";
+const assets = ["./css/index.css", "./js/index.js", "./images/oba-logo.jpg"];
+
 self.addEventListener("install", function(event) {
-    console.log("The service worker is being installed.");
-    event.waitUntil();
+    console.log("The service worker is being installed...");
+    // add assets to cache
+    event.waitUntil(addToCache());
   });
+
+self.addEventListener("fetch", function(event) {
+  console.log("The service worker is serving the asset...");
+  // respond with (give back) cache assets
+  event.respondWith(fromCache(event.request));
+  // add updated assets to cache
+  event.waitUntil(updateCache(event.request));
+});
   
-  
+async function addToCache() {
+  const cache = await openCache(CACHE);
+  return cache.addAll(assets);
+}
+
+async function fromCache(request) {
+  const cache = await openCache(CACHE);
+  const matching = await cache.match(request);
+  return matching || Promise.reject("no-match");
+}
+
+async function updateCache(request) {
+  const cache = await openCache(CACHE);
+  const response = await fetch(request);
+  return cache.put(request, response);
+}
+
+async function openCache(cacheName) {
+  return await caches.open(cacheName);
+}
